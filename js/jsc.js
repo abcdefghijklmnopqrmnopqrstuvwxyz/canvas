664fdcfe28feaec2d18ffd6eb3f356c15799982d
Erik Boháč <bohac@ERIK.bohac2@spsejecna.cz>
2023-02-01 20:36:08 +0100

    Project finished
    finished all assignments

--- js/jsc.js
+++ js/jsc.js
@@ -1,20 +1,70 @@
 const colorPick = document.getElementById("color");
-const size = document.getElementById("size");
 const erase = document.getElementById("erase");
+const load = document.getElementById("load");
 const canvas = document.getElementById("canvas");
+let ctx = canvas.getContext('2d');
+let saves = [];
 
 erase.addEventListener("click", clear);
+canvas.addEventListener("mousemove", draw);
+colorPick.addEventListener("focusout", color);
+load.addEventListener("change", loadData);
 
-canvas.addEventListener("mousemove", draw)
+
+
+function color()
+{
+    ctx.strokeStyle = colorPick.value;
+}
 
 function clear()
 {
-    
+    save();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function save()
+{
+    saves.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
+    let newSave = document.createElement("option");
+    newSave.innerHTML = "Save " + saves.length;
+    newSave.setAttribute("id", saves.length);
+    load.appendChild(newSave);
+}
+
+function loadData()
+{
+    ctx.putImageData(saves[load.options[load.selectedIndex].id - 1], 0, 0);
+}
+
+function setSize()
+{
+    ctx.canvas.width = window.innerWidth * 100 / 110;
+    ctx.canvas.height = window.innerHeight * 100 / 120;
+    setCtx();
+}
+
+function setCtx()
+{
+    ctx.lineWidth = 5;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = '#000000';
 }
 
 function draw(event)
 {
-    let x = event.clientX;
-    let y = event.clientY;
-    canvas.get
+    if (event.buttons !== 1) 
+        return;
+    ctx.beginPath();
+    ctx.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+    ctx.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+    ctx.stroke();
 }
+
+function setLineWidth(id)
+{
+    ctx.lineWidth = id;
+}
+
+window.onload = setSize;
+window.onresize = setSize;
